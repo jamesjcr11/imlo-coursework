212265c07a37e271c0ba0a53af12a8c0e8763aec
unknown <jamesrissenjr@gmail.com>
2026-04-22 14:52:06 +0100

    dataset loading

---
+++ train.py
@@ -0,0 +1,39 @@
+import numpy as np
+from PIL import Image
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import torchvision 
+import torchvision.transforms as transforms 
+import torchvision.datasets as datasets
+
+
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+
+train_data = train_data = datasets.OxfordIIITPet(
+    root="./data",
+    split="trainval",
+    download=True,
+    transform=transform,
+)
+
+test_data = datasets.OxfordIIITPet(
+    root="./data",
+    split="test",
+    download=True,
+    transform=transform,
+)
+
+train_loader = torch.utils.data.DataLoader(train_data, batch_size = 32, shuffle=True, num_workers=2)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size = 32, shuffle=False, num_workers=2)
+
+print("Train dataset size:", len(train_data))
+print("Test dataset size:", len(test_data))
