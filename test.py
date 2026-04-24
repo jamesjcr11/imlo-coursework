111e98abaaef0d49704ca9826c6ec0c593fe949e
jamesjcr11 <jamesrissenjr@gmail.com>
2026-04-24 01:39:52 +0100

    CNN Structural change + Tweaks

--- test.py
+++ test.py
@@ -46,65 +46,93 @@ def get_accuracy(model, loader):
 
 
 
+def get_accuracy(model, loader):
+    correct = 0
+    total = 0
+
+    model.eval()
+    with torch.no_grad():
+        for images, labels in loader:
+            images = images.to(device)     
+            labels = labels.to(device)     
+
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    model.train()
+    return 100 * correct / total
+
+
+
 
 class NeuralNet(nn.Module):
     def __init__(self):
         super().__init__()
 
         self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1), 
-            nn.BatchNorm2d(16),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1), 
+            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1), 
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
 
-        self.conv3 = nn.Sequential(
+        self.conv2 = nn.Sequential(
             nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1), 
-            nn.BatchNorm2d(64),     
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(2)
         )
 
-        self.conv4 = nn.Sequential(
+        self.conv3 = nn.Sequential(
             nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1), 
-            nn.BatchNorm2d(128),
+            nn.BatchNorm2d(128),     
             nn.ReLU(),
             nn.MaxPool2d(2)
-        )   
+        )
 
-        self.pool = nn.AdaptiveAvgPool2d((1, 1))
+
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1), 
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+  
+        self.pool = nn.MaxPool2d((1, 1))
+        self.gap = nn.AdaptiveAvgPool2d((4, 4))
         
-        self.fc1 = nn.Linear(128 , 512)
-        self.fc2 = nn.Linear(512, 256)
-        self.fc3 = nn.Linear(256, 37)
+        self.fc1 = nn.Linear(256 * 4 * 4,  1024)
+        self.bn1 = nn.BatchNorm1d(1024)
 
+        self.fc2 = nn.Linear(1024, 512)
+        self.bn2 = nn.BatchNorm1d(512)
+
+        self.fc3 = nn.Linear(512, 37)
+
+        self.dropout = nn.Dropout(0.1)
     
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
         x = self.conv4(x)
-        
-        x = self.pool(x)
-
+    
+                
+        x = self.gap(x)
         x = torch.flatten(x, 1)
 
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, 0.3, training=self.training)
+        x = F.relu(self.bn1(self.fc1(x)))
+        x = self.dropout(x)
 
-        x = F.relu(self.fc2(x)) 
-        x = F.dropout(x, 0.3, training=self.training)
-
+        x = F.relu(self.bn2(self.fc2(x)))
+        x = F.dropout(x)
 
         x = self.fc3(x)
         return x
+    
     
     
 
