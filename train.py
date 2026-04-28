2877267347672c79ffc026c80e5bbe3620c848b0
jamesjcr11 <jamesrissenjr@gmail.com>
2026-04-28 01:31:07 +0100

    adding Residual skip

--- train.py
+++ train.py
@@ -125,6 +125,31 @@ val_loader = torch.utils.data.DataLoader(val_data, batch_size = 64, shuffle=Fals
 
 
 
+class ConvBlock(nn.Module):
+    def __init__(self, in_c, out_c):
+        super().__init__()
+
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_c)
+
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_c)
+
+        self.skip = nn.Identity()
+        if in_c != out_c:
+            self.skip = nn.Conv2d(in_c, out_c, kernel_size=1, stride=1, padding=0)
+
+        self.pool = nn.MaxPool2d(2)
+
+    def forward(self, x):
+        identity = self.skip(x)
+
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+
+        out = F.relu(out + identity)
+        return self.pool(out)
+
 
 #///////////////////////////////////////////////////////////////
 
@@ -133,60 +158,18 @@ class NeuralNet(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.Conv2d(32, 32, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
+        self.conv1 = ConvBlock(3, 32)
+        self.conv2 = ConvBlock(32, 64)
+        self.conv3 = ConvBlock(64, 128)
+        self.conv4 = ConvBlock(128, 256)
 
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-
-
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-
-       # self.pool = nn.MaxPool2d((1, 1))
         self.gap = nn.AdaptiveAvgPool2d((4, 4))
 
-        self.fc1 = nn.Linear(256 * 4 * 4 ,  512)
-        self.bn1 = nn.BatchNorm1d(512)
-
-        #self.fc2 = nn.Linear(1024, 512)
-        #self.bn2 = nn.BatchNorm1d(512)
+        self.fc1 = nn.Linear(256 * 4* 4, 256)
+        self.bn1 = nn.BatchNorm1d(256)
 
         self.dropout = nn.Dropout(0.35)
-        self.fc2 = nn.Linear(512, 37)
-
-
+        self.fc2 = nn.Linear(256, 37)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -194,19 +177,14 @@ class NeuralNet(nn.Module):
         x = self.conv3(x)
         x = self.conv4(x)
 
-
         x = self.gap(x)
         x = torch.flatten(x, 1)
 
         x = F.relu(self.bn1(self.fc1(x)))
         x = self.dropout(x)
 
-       # x = F.relu(self.bn2(self.fc2(x)))
-        #x = self.dropout(x)
-
         x = self.fc2(x)
         return x
-
 
 
 #////////////////////////////////////////////////////////////////
@@ -214,7 +192,7 @@ class NeuralNet(nn.Module):
 net = NeuralNet().to(device)
 #loss_function = nn.CrossEntropyLoss(label_smoothing=0.1)
 loss_function = nn.CrossEntropyLoss()
-optimizer = optim.Adam(net.parameters(), lr=0.001, weight_decay=0.0005)
+optimizer = optim.Adam(net.parameters(), lr=0.001, weight_decay=0.001)
 scheduler = optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=30)
 
 
