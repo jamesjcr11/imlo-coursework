5690845eba2911829a5f551980e4b7b9d29f44fe
jamesjcr11 <jamesrissenjr@gmail.com>
2026-05-08 17:40:35 +0100

    moved testing from train.py to test.py to seperatly display test accuracy

--- test.py
+++ test.py
@@ -7,137 +7,143 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 import torchvision
-import torchvision.transforms as transforms 
+import torchvision.transforms as transforms
+from torchvision.transforms import InterpolationMode
 import torchvision.datasets as datasets
+import copy
+from torchvision.transforms import v2
+from sklearn.model_selection import StratifiedShuffleSplit
+from torch.utils.data import Subset
+
+torch.manual_seed(7)
+np.random.seed(7)   
 
 
-transform = transforms.Compose([
-    transforms.Resize((128, 128)),
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+eval_transform = transforms.Compose ([
+    transforms.Resize((200, 200)),
+    transforms.CenterCrop(180),
     transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    transforms.Normalize((0.485, 0.456, 0.406),(0.229, 0.224, 0.225))
 ])
+
+
 
 test_data = datasets.OxfordIIITPet(
     root="./data",
     split="test",
     download=True,
-    transform=transform,
+    transform=eval_transform,
 )
 
-test_loader = torch.utils.data.DataLoader(test_data, batch_size = 32, shuffle=False, num_workers=0)
-print("Test dataset size:", len(test_data))
 
-def get_accuracy(model, loader):
-    correct = 0
-    total = 0
-
-    model.eval()
-    with torch.no_grad():
-        for images, labels in loader:
-            outputs = model(images)
-            _, predicted = torch.max(outputs, 1)
-
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-    model.train()
-    return 100 * correct / total
+test_loader = torch.utils.data.DataLoader(test_data, batch_size = 64, shuffle=False, num_workers=0)
 
 
 
+class ConvBlock(nn.Module):
+    def __init__(self, in_c, out_c, do_pool=True):
+        super().__init__()
 
-def get_accuracy(model, loader):
-    correct = 0
-    total = 0
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_c)
 
-    model.eval()
-    with torch.no_grad():
-        for images, labels in loader:
-            images = images.to(device)     
-            labels = labels.to(device)     
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_c)
+        self.conv3 = nn.Conv2d(out_c, out_c, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(out_c)
 
-            outputs = model(images)
-            _, predicted = torch.max(outputs, 1)
+        self.skip = nn.Identity()
+        if in_c != out_c:
+            self.skip = nn.Sequential(
+                nn.Conv2d(in_c, out_c, kernel_size=1, stride=1, padding=0),
+                nn.BatchNorm2d(out_c)
+            )
 
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
+        self.do_pool = do_pool
+        self.pool = nn.MaxPool2d(2)
 
-    model.train()
-    return 100 * correct / total
+    def forward(self, x):
+        identity = self.skip(x)
+
+        out = F.silu(self.bn1(self.conv1(x)))
+        out = F.silu(self.bn2(self.conv2(out)))
+        out = self.bn3(self.conv3(out))
+
+        out = out + identity
+        out = F.silu(out)
+
+        if self.do_pool:
+            out = self.pool(out)
+
+        return out
 
 
+#///////////////////////////////////////////////////////////////
 
 
 class NeuralNet(nn.Module):
     def __init__(self):
         super().__init__()
 
-        self.conv1 = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1), 
-            nn.BatchNorm2d(32),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
+        self.conv1 = ConvBlock(3, 64)
+        self.conv2 = ConvBlock(64, 128)
+        self.conv3 = ConvBlock(128, 256)
+        self.conv4 = ConvBlock(256, 384)
+        self.conv5 = ConvBlock(384, 512)
 
-        self.conv2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1), 
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
+        self.gap = nn.AdaptiveAvgPool2d((2,2))
 
-        self.conv3 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1), 
-            nn.BatchNorm2d(128),     
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
+        self.fc1 = nn.Linear(512 * 2 * 2, 256)
+        self.bn1 = nn.BatchNorm1d(256)
 
+        self.dropout = nn.Dropout(0.3)
+        self.fc2 = nn.Linear(256, 37)
 
-        self.conv4 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1), 
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.MaxPool2d(2)
-        )
-  
-        self.pool = nn.MaxPool2d((1, 1))
-        self.gap = nn.AdaptiveAvgPool2d((4, 4))
-        
-        self.fc1 = nn.Linear(256 * 4 * 4,  1024)
-        self.bn1 = nn.BatchNorm1d(1024)
-
-        self.fc2 = nn.Linear(1024, 512)
-        self.bn2 = nn.BatchNorm1d(512)
-
-        self.fc3 = nn.Linear(512, 37)
-
-        self.dropout = nn.Dropout(0.1)
-    
     def forward(self, x):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
         x = self.conv4(x)
-    
-                
+        x = self.conv5(x)
+
         x = self.gap(x)
         x = torch.flatten(x, 1)
 
-        x = F.relu(self.bn1(self.fc1(x)))
+        x = F.silu(self.bn1(self.fc1(x)))
         x = self.dropout(x)
 
-        x = F.relu(self.bn2(self.fc2(x)))
-        x = F.dropout(x)
-
-        x = self.fc3(x)
+        x = self.fc2(x)
         return x
     
     
-    
 
-net = NeuralNet()
-net.load_state_dict(torch.load("trained_model.pth"))
+def get_accuracy(model, loader):
+    correct = 0
+    total = 0
+
+    model.eval()
+    with torch.no_grad():
+        for images, labels in loader:
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    model.train()
+    return 100 * correct / total
+
+
+
+net = NeuralNet().to(device)
+net.load_state_dict(torch.load("model.pth", map_location=device))
+
 test_acc = get_accuracy(net, test_loader)
-
 print(f"Test Accuracy: {test_acc:.2f}%")
